3f075fa111d3f409fd8dec399b4e9213ed7381ea
niko waflle <morcosis.otaku@gmail.com>
2025-03-18 19:52:53 -0600

    logro: vistas de cadena, vistas y store procedure

---
+++ Unidad_2/07- views transact-funciones- cadena.sql
@@ -0,0 +1,87 @@
+-- funciones de cadena
+
+-- las funciones de cadena permite manipular tipos de datos
+-- como VARCHAR, NVVARCHAR,  CHAR, NCHAR
+
+-- FUNCION LEN -> DEVUELVE LA LONGITUD DE UNA CADENA
+
+--DECLARACION DE UNA VARIABLE
+
+
+DECLARE @NUMERO INT;
+SET @NUMERO = 10;
+PRINT @NUMERO
+
+
+
+--OBTENER EL TAMAÑO DE LA CADENA ALMACENADA EN LA VARIABLE TETO
+DECLARE @TEXTO VARCHAR(50) = 'HOLA, MUNDO!'
+SELECT LEN(@TEXTO) AS [LONGITUD]
+
+SELECT CompanyName, LEN(CompanyName) AS LONGITUD FROM Customers
+ORDER BY 2 DESC
+
+-- LEFT -> EXTRAE UN NUMERO DE CARACTERES UN DETERMINADO 
+-- NUMERO DE CARACTERES DESDE EL INICIO DE LA CADENA 
+
+SELECT LEFT(@TEXTO,4) AS INICIO
+
+-- RIGHT -> EXTRAE UNNUMERO DECARACTERES DETERMINADO
+-- NUMERO DE CARACTERES DEL FINAL DE LA CADENA 
+GO
+
+DECLARE @TEXTO VARCHAR(50) = 'HOLA, MUNDO!'
+SELECT LEN(@TEXTO) AS [LONGITUD]
+SELECT RIGHT(@TEXTO,6) AS FINAL
+--SUBSTRIG -> EXTRAE UNAPARTE DE LA CADENA, 
+-- DONDE EL PRIMER PARAMETRO ES LA POSICION INICIAL Y E
+-- SEGUNDO ES EL RECORRIDO 
+
+SELECT SUBSTRING(@TEXTO,7,5) AS MEOW
+
+--RENPLACE  -> REMPLAZA UNA SUBCADENA POR OTRA 
+-- REMPLASE (STRING_EXPRESSION 2)
+SELECT REPLACE(@TEXTO, 'MUNDO', 'AMIGO')
+
+--CHARINDEX -> DA LA POSICION DEL TEXTO
+DECLARE @TEXTO2 VARCHAR(50) = 'HOLA, MUNDO!'
+SELECT CHARINDEX('MUNDO',@TEXTO2)
+
+--UPPER ->CONVIERTE UNA CADENA EN MAYUSCULAS
+go
+DECLARE @TEXTO2 VARCHAR(50) = 'HOLA, mundo!'
+
+SELECT UPPER(@TEXTO2) AS MAYUSCULAS
+SELECT UPPER(SUBSTRING(@TEXTO2,7,5)) AS MAYUSCULAS
+
+SELECT	concat(left(@TEXTO2,5 )	, ' ', UPPER(SUBSTRING(@TEXTO2,7,6))) AS MAYUSCULAS
+-- select trim ('    test     ') as result
+--	trim ->quita espacios en blanco de una cadena pero solo de derecha e izquierda
+go
+DECLARE @TEXTO2 VARCHAR(50) = '             HOLA,   mundo!            '
+select ltrim (@TEXTO2) as result
+
+select rtrim (@TEXTO2) as result
+
+select trim (@TEXTO2) as result
+go
+
+
+
+
+SELECT CompanyName, LEN(CompanyName) AS 'NUMERO DE CARACTERES',
+LEFT(CompanyName,4) AS INICIO,
+RIGHT(CompanyName,6) AS FINAL,
+SUBSTRING(COMPANYNAME,7,5) AS MEOW
+FROM Customers
+ORDER BY 2 ASC
+go
+
+select c.CompanyName, upper(c.CompanyName) from Customers as c
+
+update Customers
+set CompanyName = upper (CompanyName)
+where Country in ('mexico', 'germany') 
+
+select * from Customers
+where Country in ('mexico', 'germany') 
---
+++ Unidad_2/09- ejercicio  store prosedures ejemplo procedure BD.sql
@@ -0,0 +1,86 @@
+-- realisar un pedido 
+--validar que el pedido no exista
+-- validar que el cliente exista, el empleado y el producto exista,
+-- validar que la cantidad a vender tenga sufucuente stock 
+-- insertar el pedido y calcular el importe (multiplicando el precio del porducto
+-- por la cantidad vendida)
+-- actualizar el stock del producto (restando el stock menos la cantidad vendida)
+
+create or alter proc spu_RealizarPedido
+@numPedido int, @cliente int,
+@repre int, @fab char(3), 
+@proceducto char(5), @cantidad int
+as 
+begin
+	if exists (select 1 from Pedidos where Num_Pedido = @numPedido) -- verificamos si el pedido existe
+	begin
+	print 'el pedido ya existe'
+	return
+	end 
+	if	not exists (select 1 from Clientes where Num_Cli = @cliente) or -- verificamos si el cliente existe
+		not exists (select 1 from Representantes where Num_Empl = @repre) or
+		not exists (select 1 from Productos where Id_fab= @fab and Id_producto = @proceducto) 
+	begin
+	print 'el pedido ya existe'
+	return
+	end 
+
+	if @cantidad <= 0
+	begin 
+		print 'la cantidad no puede ser 0 o negativo'
+		return ;
+	end
+	declare @stockvalido int
+	select @stockvalido= Stock from Productos
+	where Id_fab = @fab and Id_producto = @proceducto
+
+		if @cantidad > @stockvalido
+	begin 
+		print 'no hay suficiente stock'
+		return;
+	end
+	declare @precio money
+	declare @importe money 
+	select @precio = Precio  from   Productos where Id_fab = @fab  and Id_producto = @proceducto
+	
+	set @importe= @cantidad * @precio
+
+	begin try
+	-- se inserto un pedido
+	insert into Pedidos
+	values(@numPedido, GETDATE(), @cliente, @repre, @fab, @proceducto, @cantidad,@importe)
+
+	update Productos
+	set Stock = Stock - @cantidad
+	where Id_fab = @fab and Id_producto = @proceducto;
+
+	end try
+	begin catch
+	print 'Error al actualizar datos'
+	return;
+	end catch
+end;
+
+exec spu_RealizarPedido 
+@numPedido = 112961, @cliente = 2117,
+@repre = 106, @fab = 'REI' , 
+@proceducto = '2A44L', @cantidad = 10
+
+
+exec spu_RealizarPedido 
+@numPedido = 112961, @cliente = 2117,
+@repre = 106, @fab = 'REI' , 
+@proceducto = '2A44L', @cantidad = 10
+
+
+exec spu_RealizarPedido 
+@numPedido = 112961, @cliente = 2117,
+@repre = 106, @fab = 'REI' , 
+@proceducto = '2A44L', @cantidad = 10
+
+exec spu_RealizarPedido @numpedido = 113070, @cliente = 2117,
+@repre=101, @fab = 'ACI',
+@proceducto = '4100X', @cantidad= 20
+
+select * from Productos
+where Id_fab = 'ACI' and Id_producto = '4100X'
